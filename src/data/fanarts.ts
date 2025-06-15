7064f3810a1255dbf67b4dcfd9f6790ee920f292
corecathx <cepssetiadi20@gmail.com>
2025-06-15 19:26:59 +0700

    ok

---
+++ src/data/fanarts.ts
@@ -0,0 +1,10 @@
+import wawa from "../assets/images/projects/linetapper.png";
+
+export default [
+    {
+        image: '/images/fanarts/01.png',
+        artist: 'Zep/Jet (Zepphyrion)',
+        artistUsn: 'zepphyrjet',
+        link: "https://instagram.com/zepphyrjet",
+    }
+];
