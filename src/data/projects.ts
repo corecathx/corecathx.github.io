cbc3f8f08e50818792342840127dd1904a45c555
corecathx <73734126+corecathx@users.noreply.github.com>
2025-11-03 08:11:24 +0700

    feat: random thing lol

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -2,36 +2,41 @@ import wawa from "../assets/images/projects/linetapper.png";
 
 export default [
     {
-      title: "LineTapper",
-      image: wawa.src,
-      description: "A rhythm game made by CoreCat using HaxeFlixel.",
-      link: "https://github.com/corecathx/LineTapper",
+        title: "LineTapper",
+        image: wawa.src,
+        description: "A rhythm game made by CoreCat using HaxeFlixel.",
+        link: "https://github.com/corecathx/LineTapper",
     },
     {
-      title: "FNF CDEV Engine",
-      image: "https://images.gamebanana.com/img/ss/mods/658599b87b960.jpg",
-      description:
-        "CDEV Engine is a Friday Night Funkin' Engine intended to fix issues with the base game while also adding a lot of features.",
-      link: "https://github.com/corecathx/FNF-CDEV-Engine",
+        title: "FNF CDEV Engine",
+        image: "https://images.gamebanana.com/img/ss/mods/658599b87b960.jpg",
+        description:
+            "CDEV Engine is a Friday Night Funkin' Engine intended to fix issues with the base game while also adding a lot of features.",
+        link: "https://github.com/corecathx/FNF-CDEV-Engine",
     },
     {
-      title: "Random Scripting Language",
-      image: "https://github.com/corecathx/RSL/raw/main/icon.png",
-      description:
-        "RSL is a scripting language I made for fun using Haxe. Don't expect much from this.",
-      link: "https://github.com/corecathx/rsl",
+        title: "Random Scripting Language",
+        image: "https://github.com/corecathx/RSL/raw/main/icon.png",
+        description:
+            "RSL is a scripting language I made for fun using Haxe. Don't expect much from this.",
+        link: "https://github.com/corecathx/rsl",
     },
     {
-      title: "HXDC",
-      image: 'https://raw.githubusercontent.com/corecathx/hxdc/main/res/logo.png',
-      description: 'A Discord Client made with Haxe.',
-      link: "https://github.com/corecathx/hxdc",
+        title: "HXDC",
+        image: 'https://raw.githubusercontent.com/corecathx/hxdc/main/res/logo.png',
+        description: 'A Discord Client made with Haxe.',
+        link: "https://github.com/corecathx/hxdc",
     },
     {
-      title: "Hypertext Engine",
-      image: '/images/fnfhe.png',
-      description: "An FNF Engine made entirely in HTML, CSS, and JS.",
-      link: 'https://corecathx.github.io/projects/hypertext-engine/'
-    }
-  ];
-  
+        title: "Hypertext Engine",
+        image: '/images/fnfhe.png',
+        description: "An FNF Engine made entirely in HTML, CSS, and JS.",
+        link: '/projects/hypertext-engine/'
+    },
+    {
+        title: "CoreCat's 4K Mania Skin",
+        image: '/images/osu/skinpreview1.png',
+        description: "A skin I made for osu!mania to enhance my gameplay experience.",
+        link: '/projects/osu-mania-skin/'
+    },
+];
